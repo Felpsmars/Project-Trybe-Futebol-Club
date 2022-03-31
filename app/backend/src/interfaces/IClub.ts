a1882743a3fcde8421ff7204ef924e45a169cd2c
Felipe Martins <mfelipeluiz99@gmail.com>
2022-03-30 23:59:32 -0300

    refactoring interface IClub

--- app/backend/src/interfaces/IClub.ts
+++ app/backend/src/interfaces/IClub.ts
@@ -2,3 +2,17 @@ export interface IClub {
   id: number;
   clubName: string;
 }
+
+export interface IClubSummation {
+  id?: number;
+  name: string;
+  goalsFavor: number;
+  goalsOwn: number;
+  goalsBalance: number;
+  totalDraws: number;
+  totalVictories: number;
+  totalLosses: number;
+  totalPoints: number;
+  totalGames: number;
+  efficiency?: number;
+}
